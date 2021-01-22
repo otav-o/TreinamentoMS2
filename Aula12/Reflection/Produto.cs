dc2bcebfa81b0ee82f06de1623d669d9038d7cff
otav-o <otaviocacadordioscanio@gmail.com>
2021-01-22 19:18:23 -0300

    Início aula 12

---
+++ Aula12/Reflection/Produto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Reflection
+{
+    class Produto
+    {
+        public int Codigo { get; set; }
+        public string Descricao { get; set; }
+        public double Preco { get; set; }
+
+    }
+}
---
+++ Aula12/Reflection/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Reflection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            object obj = new Produto { Codigo = 1, Descricao = "Coca-cola", Preco = 5.5 };
+
+            var tipo = obj.GetType(); // como a classe não herda de ninguém, ela herda de Object
+                // pega o tipo do objeto mesmo (Produto) e não o da variável obj (Object)
+
+            Console.WriteLine($"Foi instanciado um objeto da classe '{tipo.Name}'");
+            /*
+            if (typeof(int) == idade.GetType()) // todo objeto em C# herda de Object, logo esse método é possível
+                Console.WriteLine("A variável 'idade' é do tipo 'int'");
+            else
+                Console.WriteLine("A variável 'idade' não é do tipo 'int'");
+            */
+
+            Console.ReadKey();
+        }
+    }
+}
