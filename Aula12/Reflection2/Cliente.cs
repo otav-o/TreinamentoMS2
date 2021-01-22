d22ad71789c6244476a2e7abd0fa920c405a2049
otav-o <otaviocacadordioscanio@gmail.com>
2021-01-22 20:06:21 -0300

    prop.SetValue()

---
+++ Aula12/Reflection2/Cliente.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Reflection2
+{
+    class Cliente
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+
+    }
+}
---
+++ Aula12/Reflection2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Reflection2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var objeto1 = new Produto { Codigo = 1, Descricao = "Coca-cola", Preco = 5.5 };
+            var tipo = objeto1.GetType();
+
+            var prop = tipo.GetProperty("Descricao"); // interessante!
+
+            Console.WriteLine($"{prop.Name}: {prop.GetValue(objeto1)}"); // isso já foi feito no outro projeto
+
+            prop.SetValue(objeto1, "Coca-Cola 1L"); // para atribuir o valor a uma propriedade
+
+            Console.WriteLine($"{prop.Name}: {prop.GetValue(objeto1)}");
+
+            Console.WriteLine($"Nova descrição: {objeto1.Descricao}");
+
+            Console.ReadLine();
+        }
+    }
+}
