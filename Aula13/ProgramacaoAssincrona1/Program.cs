cb6bbfa8ebaa3ad255310e031084e86758d8d77f
otav-o <otaviocacadordioscanio@gmail.com>
2021-01-22 22:41:38 -0300

    Uso de Async, Task<T>, await, Task.Run(Action)

--- Aula13/ProgramacaoAssincrona1/Program.cs
+++ Aula13/ProgramacaoAssincrona1/Program.cs
@@ -12,6 +12,7 @@ namespace ProgramacaoAssincrona1
         {
             Console.WriteLine($"Número: {ProcessamentoSincrono()} raízes inteiras de 0 a 1 bilhão."); // demorou cerca de 8 segundos para processar de forma síncrona
             Console.WriteLine($"Número: {ProcessamentoAssincrono()}"); // 2.5 segundos!
+            Console.WriteLine($"Número: {ProcessamentoAssincronoUsandoTask()}"); // 3.5s
         }
 
         private static int ProcessamentoAssincrono()
@@ -32,6 +33,50 @@ namespace ProgramacaoAssincrona1
             Console.WriteLine($"Assíncrono: {(DateTime.Now - inicio).TotalSeconds} segundos.");
             
             return tot;
+        }
+
+        private static int ProcessamentoAssincronoUsandoTask()
+        {
+            var div = MAX / NUMERO_THREADS;
+            var inicio = DateTime.Now;
+
+            var resultados = new Task<int>[NUMERO_THREADS]; // vetor de tasks
+
+            int tot;
+
+            for (int i = 0; i < NUMERO_THREADS; i++) // muito parecido com o ParallelFor
+            {
+                resultados[i] = ProcessamentoAsync(i * div, (i + 1) * div); // retorna um Task<int> e não um int
+                // cada resultado é uma tarefa em execução
+                // ele não tem o resultado ainda, e sim uma tarefa
+            }
+
+            Task.WaitAll(resultados); // dá um Wait em cada uma das tarefas (aguarda o término delas, pausa a execução)
+
+            tot = resultados.Sum(x => x.Result); // toda Task tem um Result - é uma propriedade (não dá para somar tasks)
+
+            Console.WriteLine($"Assíncrono com Task: {(DateTime.Now - inicio).TotalSeconds} segundos");
+
+            return tot;
+        }
+
+        private async static Task<int> ProcessamentoAsync(int inicio, int fim) // colocar async na frente do nome: não bloqueia o programa
+        {
+            int cont = 0;
+            double raiz;
+
+            await Task.Run(() =>
+            {
+                for (int i = inicio; i < fim; i++)
+                {
+                    raiz = Math.Sqrt(i);
+                    if (raiz == Math.Floor(raiz))
+                        cont++;
+                }
+            });
+            // quando chega no await, a execução sai e retorna um objeto Task
+
+            return cont;
         }
 
         private static int ProcessamentoSincrono()
