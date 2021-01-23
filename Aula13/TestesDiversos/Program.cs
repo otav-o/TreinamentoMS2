e3a477d889f9b4775c40c2796cc901d7260c3f1e
otav-o <otaviocacadordioscanio@gmail.com>
2021-01-22 21:42:26 -0300

    Início da explicação com DateTime e TimeSpan

---
+++ Aula13/TestesDiversos/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TestesDiversos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dataInicial = new DateTime(2020, 1, 19, 20, 21, 22);
+            var dataFinal = DateTime.Now;
+
+            TimeSpan intervaloDeTempo = (dataFinal - dataInicial); // TimeSpan: intervalo de tempo
+
+            Console.WriteLine($"Data inicial: {dataInicial.ToShortDateString()} {dataInicial.ToLongTimeString()}");
+            Console.WriteLine($"Data final: {dataFinal.ToShortDateString()} {dataFinal.ToLongTimeString()}");
+
+            Console.WriteLine($"Diferença em anos: {intervaloDeTempo.TotalDays}");
+        }
+    }
+}
