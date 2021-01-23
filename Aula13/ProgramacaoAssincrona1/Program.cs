f6d764b2ef778db2e8f3e4acd31f936ce1ba2c6d
otav-o <otaviocacadordioscanio@gmail.com>
2021-01-22 21:50:57 -0300

    Uso da programação síncrona

---
+++ Aula13/ProgramacaoAssincrona1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ProgramacaoAssincrona1
+{
+    class Program
+    {
+        private const int MAX = 1000000000;
+        private const int NUMERO_THREADS = 10;
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Número: {ProcessamentoSincrono()} raízes inteiras de 0 a 1 bilhão."); // demorou cerca de 8 segundos para processar de forma síncrona
+        }
+
+        private static int ProcessamentoSincrono()
+        {
+            DateTime inicio = DateTime.Now;
+            int cont = Processamento(0, MAX);
+            Console.WriteLine($"Síncrono: {(DateTime.Now - inicio).TotalSeconds} segundos.");
+            return cont;
+        }
+
+        private static int Processamento(int inicio, int fim)
+        { // descobrir quantos números de 0 a 1 bilhão possuem a raíz inteira
+            int cont = 0;
+            double raiz;
+            for (int i = inicio; i < fim; i++)
+            {
+                raiz = Math.Sqrt(i);
+                if (raiz == Math.Floor(raiz)) // arredonda para baixo
+                    cont++;
+            }
+            return cont;
+        }
+    }
+}
