7d4acbb4b8ba4029774f719fc6a332933ea58163
otav-o <otaviocacadordioscanio@gmail.com>
2021-01-24 22:11:06 -0300

    Criação do método Inserir()

---
+++ Aula16/CamadaAcessoADados/DAOs/ProdutoDAO.cs
@@ -0,0 +1,51 @@
+﻿using CamadaAcessoADados.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace CamadaAcessoADados.DAOs
+{
+    class ProdutoDAO
+    {
+        public void Inserir(Produto produto)
+        {
+            using(var conexao = new SqlConnection(strConexao))
+            {
+                conexao.Open();
+                var sql = "insert into produto " +
+                    "(id_produto, codigo, descricao, preco)" +
+                    "values(@id_produto, @codigo, @descricao, @preco)";
+
+                var cmd = new SqlCommand(sql, conexao); // cria o command
+
+                cmd.Parameters.AddWithValue("id_produto", produto.ProdutoId); // adiciona o id do objeto produto como parâmetro @id_produto
+                cmd.Parameters.AddWithValue("codigo", produto.Codigo);
+                cmd.Parameters.AddWithValue("descricao", produto.Descricao);
+                cmd.Parameters.AddWithValue("preco", produto.Preco);
+
+                cmd.ExecuteNonQuery();
+
+                conexao.Close();
+            }
+        }
+        public void Atualizar(Produto produto)
+        {
+
+        }
+        public void Excluir(Produto produto)
+        {
+
+        }
+        public Produto RetornarPorId(string id)
+        {
+            throw new NotImplementedException();
+        }
+        public IEnumerable<Produto> RetornarPorParteNome(string parteNome)
+        {
+            throw new NotImplementedException();
+        }
+
+        private string strConexao = @"Data Source=OTAVIO-VIVOBOOK\SQLEXPRESS;Initial Catalog=Aula16-TestesManager;Integrated Security=SSPI;";
+    }
+}
---
+++ Aula16/CamadaAcessoADados/Models/Produto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CamadaAcessoADados.Models
+{
+    class Produto
+    {
+        public string ProdutoId { get; set; }
+        public int Codigo { get; set; }
+        public string Descricao { get; set; }
+        public double Preco { get; set; }
+
+    }
+}
