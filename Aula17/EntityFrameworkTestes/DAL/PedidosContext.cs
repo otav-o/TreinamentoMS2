ae869459c15270064eb1fa3d9afd1655360da673
otav-o <otaviocacadordioscanio@gmail.com>
2021-01-25 20:05:28 -0300

    Métodos Incluir, alterar e excluir

--- Aula17/EntityFrameworkTestes/DAL/PedidosContext.cs
+++ Aula17/EntityFrameworkTestes/DAL/PedidosContext.cs
@@ -8,8 +8,10 @@ namespace EntityFrameworkTestes.DAL
 {
     class PedidosContext : DbContext
     {
-        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Produto> Produtos { get; set; } 
+        // DbSet é uma coleção especial do Entity em que se consegue passar o tipo da entidade que se vai mapear
+        // consegue fazer consultas e adicionar dados de forma simples
 
-        protected override void OnConfiguring(DbContextOptionsBuilder options) => options.UseSqlServer(@"Integrated Security=SSPI;Trusted_Connection=true;Persist Security Info=False;Initial Catalog=TestesEntityManager;Data Source=localhost\SQLEXPRESS");
+        protected override void OnConfiguring(DbContextOptionsBuilder options) => options.UseSqlServer(@"Integrated Security=SSPI;Trusted_Connection=true;Persist Security Info=False;Initial Catalog=TestesEntityManager;Data Source=OTAVIO-VIVOBOOK\SQLEXPRESS");
     }
 }
--- Aula17/EntityFrameworkTestes/Program.cs
+++ Aula17/EntityFrameworkTestes/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using EntityFrameworkTestes.DAL;
+using EntityFrameworkTestes.Models;
+using System;
+using System.Linq;
 
 namespace EntityFrameworkTestes
 {
@@ -6,7 +9,67 @@ namespace EntityFrameworkTestes
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //IncluirProdutos();
+            //AlterarProduto();
+            ExcluirProduto();
+
+            Console.WriteLine("Fim do programa");
+            Console.ReadKey();
+        }
+
+        private static void ExcluirProduto()
+        {
+            using (var db = new PedidosContext())
+            {
+                var p2 = (from x in db.Produtos where x.ProdutoId == "Produto2" select x).FirstOrDefault();
+
+                if (p2 == null)
+                    Console.WriteLine("Produto de id='Produto2' não encontrado");
+                else
+                {
+                    db.Produtos.Remove(p2);
+                    db.SaveChanges();
+                }
+            }
+        }
+
+        private static void AlterarProduto()
+        {
+            using (var db = new PedidosContext())
+            {
+                //var p2 = db.Produtos.Where(x => x.ProdutoId == "Produto2").FirstOrDefault();
+                var p2 = (from x in db.Produtos where x.ProdutoId == "Produto2" select x).FirstOrDefault(); // comando sql deixa de ser uma string e passa a ser verificado pelo compilador. Tem boa integração com o LINQ
+
+                p2.Descricao = "Produto 2 (Alterado)";
+                db.SaveChanges();
+            }
+        }
+
+        private static void IncluirProdutos()
+        {
+            using (var db = new PedidosContext())
+            {
+                var p1 = new Produto { ProdutoId = "Produto1", Codigo = 1, Descricao = "Produto 1", Preco = 1.11 };
+                var p2 = new Produto { ProdutoId = "Produto2", Codigo = 2, Descricao = "Produto 2", Preco = 2.22 };
+                var p3 = new Produto { ProdutoId = "Produto3", Codigo = 3, Descricao = "Produto 3", Preco = 3.33 };
+                var p4 = new Produto { ProdutoId = "Produto4", Codigo = 4, Descricao = "Produto 4", Preco = 4.44 };
+                var p5 = new Produto { ProdutoId = "Produto5", Codigo = 5, Descricao = "Produto 5", Preco = 5.55 };
+
+                db.Add(p1); // db é PedidosContext(). Adiciona-se a ele p1, vai para o DbSet<Produto> Produtos
+                db.SaveChanges();
+
+                db.Add(p2);
+                db.SaveChanges();
+
+                db.Add(p3);
+                db.SaveChanges();
+
+                db.Add(p4);
+                db.SaveChanges();
+
+                db.Add(p5);
+                db.SaveChanges();
+            }
         }
     }
 }
