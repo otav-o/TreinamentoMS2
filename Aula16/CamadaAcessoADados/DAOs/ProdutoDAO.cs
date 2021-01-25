230309fc20c888b915e46fb1af2b8bf24b5c920c
otav-o <otaviocacadordioscanio@gmail.com>
2021-01-25 09:41:20 -0300

    Criação dos métodos ExecutarComando e AdicionarParametros para melhorar o código

--- Aula16/CamadaAcessoADados/DAOs/ProdutoDAO.cs
+++ Aula16/CamadaAcessoADados/DAOs/ProdutoDAO.cs
@@ -13,50 +13,29 @@ namespace CamadaAcessoADados.DAOs
             if (produto.ProdutoId == null)
                 produto.ProdutoId = Guid.NewGuid().ToString(); // geração de número único no mundo
                                                                // melhor do que o id auto-increment pois precisaria buscar o último id gerado
-            using (var conexao = new SqlConnection(strConexao))
-            {
-                conexao.Open();
-                var sql = "insert into produto " +
-                    "(id_produto, codigo, descricao, preco)" +
-                    "values(@id_produto, @codigo, @descricao, @preco)";
+            var sql = "insert into produto " +
+                "(id_produto, codigo, descricao, preco)" +
+                "values(@id_produto, @codigo, @descricao, @preco)";
 
-                var cmd = new SqlCommand(sql, conexao); // cria o command
+            ExecutarComando(sql, produto, AdicionarParametrosTodos);
 
-                cmd.Parameters.AddWithValue("id_produto", produto.ProdutoId); // adiciona o id do objeto produto como parâmetro @id_produto
-                cmd.Parameters.AddWithValue("codigo", produto.Codigo);
-                cmd.Parameters.AddWithValue("descricao", produto.Descricao);
-                cmd.Parameters.AddWithValue("preco", produto.Preco);
-
-                cmd.ExecuteNonQuery();
-
-                conexao.Close();
-            }
         }
         public void Atualizar(Produto produto)
         {
-            using (var conexao = new SqlConnection(strConexao))
-            {
-                conexao.Open();
-                var sql = "update produto " +
-                    " set codigo=@codigo, descricao=@descricao, preco=@preco)" +
-                    " where id_produto=@id_produto";
 
-                var cmd = new SqlCommand(sql, conexao); // cria o command
+            var sql = "update produto " +
+                " set codigo=@codigo, descricao=@descricao, preco=@preco)" +
+                " where id_produto=@id_produto";
 
-                cmd.Parameters.AddWithValue("id_produto", produto.ProdutoId); // adiciona o id do objeto produto como parâmetro @id_produto
-                cmd.Parameters.AddWithValue("codigo", produto.Codigo);
-                cmd.Parameters.AddWithValue("descricao", produto.Descricao);
-                cmd.Parameters.AddWithValue("preco", produto.Preco);
-
-                cmd.ExecuteNonQuery();
-
-                conexao.Close();
-            }
-
+            ExecutarComando(sql, produto, AdicionarParametrosTodos);
+           
         }
         public void Excluir(Produto produto)
         {
+            var sql = "delete from produto" +
+                " where id_produto=@id_produto";
 
+            ExecutarComando(sql, produto, AdicionarParametroId);
         }
         public Produto RetornarPorId(string id)
         {
@@ -66,6 +45,33 @@ namespace CamadaAcessoADados.DAOs
         {
             throw new NotImplementedException();
         }
+        private void ExecutarComando(string sql, Produto obj, Action<SqlCommand, Produto> AdicionarParametros)
+        {
+            using(var conexao = new SqlConnection(strConexao))
+            {
+                conexao.Open();
+                var cmd = new SqlCommand(sql, conexao);
+
+                AdicionarParametros(cmd, obj);
+
+                cmd.ExecuteNonQuery();
+                conexao.Close();
+            }
+        }
+
+        private void AdicionarParametrosTodos(SqlCommand cmd, Produto obj)
+        {
+            cmd.Parameters.AddWithValue("id_produto", obj.ProdutoId); // adiciona o id do objeto produto como parâmetro @id_produto
+            cmd.Parameters.AddWithValue("codigo", obj.Codigo);
+            cmd.Parameters.AddWithValue("descricao", obj.Descricao);
+            cmd.Parameters.AddWithValue("preco", obj.Preco);
+        }
+        private void AdicionarParametroId(SqlCommand cmd, Produto obj)
+        {
+            cmd.Parameters.AddWithValue("codigo", obj.ProdutoId);
+        }
+
+
 
         private string strConexao = @"Data Source=OTAVIO-VIVOBOOK\SQLEXPRESS;Initial Catalog=Aula16-TestesManager;Integrated Security=SSPI;";
     }
