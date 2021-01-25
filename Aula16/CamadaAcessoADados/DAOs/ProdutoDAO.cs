0f34db4e95c13e62eaa0eda82645da8784c251be
otav-o <otaviocacadordioscanio@gmail.com>
2021-01-25 10:06:10 -0300

    Instanciando um objeto a partir de dados do banco usando DataTable

--- Aula16/CamadaAcessoADados/DAOs/ProdutoDAO.cs
+++ Aula16/CamadaAcessoADados/DAOs/ProdutoDAO.cs
@@ -1,6 +1,7 @@
 ﻿using CamadaAcessoADados.Models;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Text;
 
@@ -39,7 +40,35 @@ namespace CamadaAcessoADados.DAOs
         }
         public Produto RetornarPorId(string id)
         {
-            throw new NotImplementedException();
+            string sql = "select * from produto" +
+                "where id_produto = @id";
+            using (var conexao = new SqlConnection(strConexao))
+            {
+                var cmd = new SqlCommand(sql, conexao);
+                cmd.Parameters.AddWithValue("id_produto", id);
+
+                var registros = new DataTable();
+
+                var da = new SqlDataAdapter(cmd);
+
+                da.Fill(registros); // da preenche o DataTable
+
+                conexao.Close();
+
+                if (registros.Rows.Count == 0) // se não encontrar um produto
+                    return null;
+
+                var primeiraLinha = registros.Rows[0];
+
+                var obj = new Produto();
+                obj.ProdutoId = registros.Rows[0]["id_produto"].ToString(); // converter para string pois DT retorna um object
+                obj.Codigo = Convert.ToInt32(primeiraLinha["codigo"]); // ou registros.Rows[0]
+                obj.Descricao = primeiraLinha["descricao"].ToString();
+                obj.Preco = Convert.ToDouble(registros.Rows[0]["preco"]);
+
+                return obj; // retorna o produto
+            }
+            
         }
         public IEnumerable<Produto> RetornarPorParteNome(string parteNome)
         {
