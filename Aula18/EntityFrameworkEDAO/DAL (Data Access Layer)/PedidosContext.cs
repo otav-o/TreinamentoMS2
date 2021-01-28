4e2779a3b9fece450ef3f609ed31419d62896b30
otav-o <otaviocacadordioscanio@gmail.com>
2021-01-28 20:19:32 -0300

    Criação do DbContext

---
+++ Aula18/EntityFrameworkEDAO/DAL (Data Access Layer)/PedidosContext.cs
@@ -0,0 +1,21 @@
+﻿using EntityFrameworkEDAO.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EntityFrameworkEDAO.DAL__Data_Access_Layer_
+{
+    class PedidosContext : DbContext
+    {
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Pedido> Pedidos { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<PedidoItem> PedidoItens { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Integrated Security=SSPI;Trusted_Connection=true;Persist Security Info=False;Initial Catalog=TestesEntityManager;Data Source=localhost\SQLEXPRESS");
+        }
+    }
+}
