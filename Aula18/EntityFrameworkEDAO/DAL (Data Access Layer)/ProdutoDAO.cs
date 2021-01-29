3dc7f935d5336da4ef7f2db9d56825a08b05b6f0
otav-o <otaviocacadordioscanio@gmail.com>
2021-01-29 13:13:55 -0300

    Testando o programa

---
+++ Aula18/EntityFrameworkEDAO/DAL (Data Access Layer)/ProdutoDAO.cs
@@ -0,0 +1,57 @@
+﻿using EntityFrameworkEDAO.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EntityFrameworkEDAO.DAL__Data_Access_Layer_
+{
+    class ProdutoDAO
+    {
+        public void Inserir(Produto obj)
+        {
+            using (var contexto = new PedidosContext())
+            {
+                contexto.Produtos.Add(obj);
+                contexto.SaveChanges();
+            }
+        }
+        public void Alterar(Produto obj)
+        {
+            using (var contexto = new PedidosContext())
+            {
+                contexto.Update(obj);
+                contexto.SaveChanges();
+            }
+        }
+        public void Excluir(Produto obj)
+        {
+            using (var contexto = new PedidosContext())
+            {
+                contexto.Remove(obj);
+                contexto.SaveChanges();
+            }
+        }
+        public IList<Produto> RetornarTodos()
+        {
+            List<Produto> retorno;
+            using (var contexto = new PedidosContext())
+            {
+                retorno = contexto.Produtos.ToList();
+            }
+            return retorno;
+        }
+        public Produto RetornarPorId(string id)
+        {
+            Produto produto;
+            using (var contexto = new PedidosContext())
+            {
+                produto = contexto.Produtos.Where(x => x.ProdutoId == id).FirstOrDefault();
+            }
+            return produto;
+        }
+    }
+
+    // é bom ter uma DAO específica com um where
+    // DAO: fazer 4 operações. Toda a lógica está na classe, não estará espalhada pelo código
+}
--- Aula18/EntityFrameworkEDAO/Program.cs
+++ Aula18/EntityFrameworkEDAO/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using EntityFrameworkEDAO.DAL__Data_Access_Layer_;
+using EntityFrameworkEDAO.Models;
+using System;
 
 namespace EntityFrameworkEDAO
 {
@@ -6,7 +8,20 @@ namespace EntityFrameworkEDAO
     {
         static void Main(string[] args)
         {
-            
+            var dao = new ProdutoDAO();
+            var obj = new Produto { ProdutoId = "123", Codigo = 1, Descricao = "Coca-cola", Preco = 5.5 };
+            dao.Inserir(obj);
+
+            obj.Descricao = "Coca-Cola Lata";
+            dao.Alterar(obj);
+
+            dao.Inserir(new Produto { ProdutoId = "124", Descricao = "Pepsi", Codigo = 2, Preco = 5 });
+
+            var listaProdutos = dao.RetornarTodos();
+            foreach (var p in listaProdutos)
+                Console.WriteLine($"{p.Codigo} - {p.Descricao} - {p.Preco}");
+
+            Console.ReadKey();
         }
     }
 }
