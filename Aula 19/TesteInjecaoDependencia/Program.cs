8cd4249ecdd0fd119e2c8ba4a453d4c451faa287
otav-o <otaviocacadordioscanio@gmail.com>
2021-02-18 11:03:15 -0300

    Criação de RecepcaoIndireta

--- Aula 19/TesteInjecaoDependencia/Program.cs
+++ Aula 19/TesteInjecaoDependencia/Program.cs
@@ -12,7 +12,9 @@ namespace TesteInjecaoDependencia
 
             nome = Console.ReadLine();
 
-            var recepcao = Instancia.Get<IRecepcao>();
+            var recepcao = Instancia.Get<IRecepcao>(); // agora, até a classe program desconhece totalmente a recepção
+            // classe recepção desconhece a saudação
+            // a mágica está em configurar em Instancia, em apenas um ponto do códigoa
 
             recepcao.Recepcionar(nome);
         }
---
+++ Aula 19/TesteInjecaoDependencia/RecepcaoIndireta.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TesteInjecaoDependencia
+{
+    class RecepcaoIndireta : IRecepcao
+    {
+        public RecepcaoIndireta(ISaudacao saudacao) => this.saudacao = saudacao;
+        public void Recepcionar(string nome)
+        {
+            Cumprimentar(nome);
+            Direcionar(nome);
+        }
+
+        private void Cumprimentar(string nome)
+        {
+            saudacao.Realizar(nome);
+        }
+
+        private void Direcionar(string nome)
+        {
+            if (nome == null || nome.Length < 7)
+                Console.WriteLine("Por favor, entre na fila à sua direita ->");
+            else
+                Console.WriteLine("<- Por favor, entre na fila à sua esquerda");
+        }
+
+        private ISaudacao saudacao;
+    }
+}
